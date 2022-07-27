f91bdb6101ad3d76df8f2d173a500c57695bc4eb
avataraustin <39737076+avataraustin@users.noreply.github.com>
2022-07-27 20:40:49 +0000

    removed flagged comments from potential errors

--- main.py
+++ main.py
@@ -18,18 +18,18 @@ def hangman(word):
     msg = "Guess a letter:"
     char = input(msg)
     if char in rletters:
-      cind = rletters.index(char)   #????
+      cind = rletters.index(char)   
       board[cind] = char
       rletters[cind] = '$'
     else:
       wrong += 1
-    print(("".join(board))) #??
+    print(("".join(board))) 
     e = wrong + 1
     print("\n"
          .join(stages[0:e]))
     if "__" not in board:
       print("You win!")
-      print("".join(board))  #?
+      print("".join(board))  
       win = True
       break
   if not win:
